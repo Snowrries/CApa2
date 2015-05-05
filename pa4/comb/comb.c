f83b196499f358391146f696fbb4c620a06d66f9
Snowrries <Snowrries@users.noreply.github.com>
2015-05-05 17:04:37 -0400

    Update comb.c

--- pa4/comb/comb.c
+++ pa4/comb/comb.c
@@ -243,7 +243,7 @@ int main(int argc, char* argv[]){
 				gateout[0]->value = mux[(binary_to_gs_to_dec(gatein, numin))];
 			}
 			else{//Hopefully eats the line.
-				fscanf(cdf, "%*s\n");
+				fscanf(cdf, "%*s %*d %*s\n");
 			}
 		}
 		for(i = 0; i < (outno-1); i++){
